8e41beedb2d6c2d10be56ee1a6fd9c92277ade35
abhisekghosh-ag <abhisekghoshnormal@gmail.com>
2023-02-25 23:45:26 +0530

    Assignment Two Completed

--- DOM Assignment 2.0 1,2,3/about/about.js
+++ DOM Assignment 2.0 1,2,3/about/about.js
@@ -9,3 +9,21 @@ accordian.forEach((element) => {
     }
   });
 });
+
+// assignment two task 1 : change the bg color of about section's heading and paragraph
+
+let accordianHeading = document.querySelectorAll(".accordian h3");
+let accordianPara = document.querySelectorAll(".accordian p");
+accordianHeading.forEach((e) => {
+  e.style.backgroundColor = "#dadaf8"
+});
+accordianPara.forEach((e) => {
+  e.style.backgroundColor = "#eeeeff"
+});
+
+
+// assignment two task 2 : Change the Hobbies section 
+
+let hobbies = document.querySelectorAll(".accordian p");
+hobbiesText = hobbies[hobbies.length-1];
+hobbiesText.innerText = "I passes a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the github."
